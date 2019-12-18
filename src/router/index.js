2830a2308ee0be8e7a7e442eaf48129d4ed26c5a
fengming <1144753090@qq.com>
2019-12-18 16:41:17 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home'
+import Login from '../views/login'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(VueRouter)
 Vue.use(ElementUI)
+
 const routes = [
   {
     path: '/',
+    redirect: '/home'
+  },
+  {
+    // home主页
+    path: '/home',
     name: 'home',
     component: Home
+  }, {
+    // 登录页
+    path: '/login',
+    component: Login
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 
 ]
 
