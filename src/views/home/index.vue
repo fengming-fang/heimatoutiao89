5eb475fe3b9b97c6fdee091e9d7eda62a9250170
fengming <1144753090@qq.com>
2019-12-19 16:25:27 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,37 @@
 <template>
-<div>
-主页
-</div>
+<el-container>
+
+<el-aside>
+<!-- 使用左侧组件 -->
+<layout-aside>
+
+</layout-aside>
+</el-aside>
+
+<el-container>
+<!-- 右侧区域 -->
+  <el-header>
+右侧头部
+
+  </el-header>
+
+  <el-main>
+右侧主体
+
+  </el-main>
+</el-container>
+
+</el-container>
 </template>
 
 <script>
-export default {
+import layoutAside from '../../components/home/layout-aside' // 引入
 
+export default {
+  // 局部注册
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
