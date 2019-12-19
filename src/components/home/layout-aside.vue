c7e7a07b06e0c7370133ea9dfff80c2925d8882f
fengming <1144753090@qq.com>
2019-12-19 17:28:39 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -1,6 +1,35 @@
 <template>
-<div>
-    左侧导航
+<div class="layout-aside">
+  <div class="title">
+       <img src="../../assets/img/logo_admin.png" alt="">
+  </div>
+    <el-menu background-color="#353b4e" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu-item>
+            首页
+        </el-menu-item>
+        <!-- 第一个二级导航 -->
+        <el-submenu>
+              <template slot="title">
+                 <span> 内容管理</span>
+              </template>
+              <el-menu-item>发布文章</el-menu-item>
+              <el-menu-item>内容列表</el-menu-item>
+              <el-menu-item>评论列表</el-menu-item>
+              <el-menu-item>素材管理</el-menu-item>
+        </el-submenu>
+         <el-submenu>
+              <template slot="title">
+                 <span> 粉丝管理</span>
+              </template>
+              <el-menu-item>图文数据</el-menu-item>
+              <el-menu-item>粉丝概括</el-menu-item>
+              <el-menu-item>粉丝画像</el-menu-item>
+              <el-menu-item>粉丝列表</el-menu-item>
+        </el-submenu>
+          <el-menu-item>
+            账户信息
+        </el-menu-item>
+    </el-menu>
 </div>
 </template>
 
@@ -10,6 +39,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang=less secped>
+.layout-aside{
+  width: 231px;
+  .title{
+background-color: #2e2f32;
+text-align: center;
+padding: 10px 0;
+    img {
+      height: 35px
+    }
+}
+}
 
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,16 @@
 <template>
 <el-container>
 
-<el-aside>
 <!-- 使用左侧组件 -->
+<el-aside style="background-color:#353b4e;height:100vh;width:230px">
+<!-- 导航组件 -->
 <layout-aside>
 
 </layout-aside>
 </el-aside>
 
 <el-container>
+
 <!-- 右侧区域 -->
   <el-header>
 右侧头部
