e145c72719d0ad72881b684c251deba2a4fe4384
fengming <1144753090@qq.com>
2019-12-22 15:31:47 +0800

    黑马头条PC-评论列表-列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -24,6 +24,16 @@
          </el-table-column>
 
        </el-table>
+            <!-- 分页组件 -->
+        <el-row type='flex' justify="center" align="middle" style="height:80px">
+        <el-pagination background layout="prev, pager, next"
+        :current-page="page.currentPage"
+        :page-size="page.pageSize"
+        :total="page.total"
+        @current-change="changePage"
+        ></el-pagination>
+        </el-row>
+
   </el-card>
 </template>
 
@@ -31,10 +41,24 @@
 export default {
   data () {
     return {
-      list: []
+
+      list: [],
+      page: {
+        // 专门放置分页数据
+        total: 0, // 数据总条数
+        pageSize: 10, // 默认每页10条
+        currentPage: 1 // 当前页码 默认第一页
+      }
     }
   },
   methods: {
+
+    // 页码改变事件
+    changePage (newPage) {
+      //  修改当前页码
+      this.page.currentPage = newPage
+      this.getComment()
+    },
 
     // 打开或者关闭
     closeOrOpen (row) {
@@ -58,10 +82,17 @@ export default {
       // query 参数 / 路由参数 地址参数 get参数  axios  params
       // body参数 给 data
       // 身份信息 headers
-      this.$axios({ url: '/articles', params: { response_type: 'comment' } }).then(result => {
+      this.$axios({ url: '/articles',
+        params: { response_type: 'comment',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
+        }
+      }).then(result => {
         this.list = result.data.results // 获取评论列表数据给本身data
+        this.page.total = result.data.total_count
       })
     },
+
     // 定义一个布尔值转化方法
     formatterBool (row, column, cellValue, index) {
       // row  当前行数据
@@ -71,10 +102,12 @@ export default {
       return cellValue ? '正常' : '关闭'
     }
   },
+
   created () {
     // 调用请求数据方法
     this.getComment()
   }
+
 }
 </script>
 
