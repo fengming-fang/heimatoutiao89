853b194ffef5a171af2f423dda5d26379322117f
fengming <1144753090@qq.com>
2019-12-30 17:19:00 +0800

    黑马头条PC-请求模块单独优化方案

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -108,6 +108,7 @@
 </template>
 
 <script>
+import { getArticles, getChannels } from '../../actions/articles'
 export default {
   data () {
     return {
@@ -226,17 +227,12 @@ export default {
 
     //   获取频道
     async getChannels () {
-      let result = await this.$axios({
-        url: '/channels'
-      })
+      let result = await getChannels() // 调用频道数据
       this.channels = result.data.channels // 获取频道数据
     },
     // 获取文章列表数据
     async getArticles (params) {
-      let result = await this.$axios({
-        url: '/articles', // 请求地址
-        params
-      })
+      let result = await getArticles(params) // 调用模块的请求数据
       this.list = result.data.results // 接收文章列表数据
       this.page.total = result.data.total_count // 文章总数
     }
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -42,6 +42,7 @@
 </template>
 
 <script>
+import { getChannels } from '../../actions/articles'
 export default {
   data () {
     return {
@@ -122,12 +123,9 @@ export default {
     },
 
     //   获取频道
-    getChannels () {
-      this.$axios({
-        url: '/channels'
-      }).then(result => {
-        this.channels = result.data.channels // 获取频道数据
-      })
+    async getChannels () {
+      let result = await getChannels()
+      this.channels = result.data.channels // 获取频道数据
     },
 
     // 发布文章
