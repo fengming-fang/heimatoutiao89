e69abe3a752566356bbd5173a42616346341a7da
fengming <1144753090@qq.com>
2019-12-30 11:57:09 +0800

    黑马头条PC-echarts在项目中的应用

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,12 +1,59 @@
 <template>
 <!-- 二级路由 -->
   <div class="home">
-
+      <!-- 图表的盒子 -->
+      <div ref="myChart" class='echarts'></div>
+      <!-- <div ref="myChart2" class='echarts'></div> -->
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
+
+  mounted () {
+    // console.log(this.$refs.myChart)
+    let myChart2 = echarts.init(this.$refs.myChart) // 获取到实例对象
+
+    // let myChart2 = echarts.init(this.$refs.myChart2) // 获取到实例对象
+    // 第一个echerts的数据
+    var data = []
+    for (var i = 0; i <= 100; i++) {
+      var theta = i / 100 * 360
+      var r = 5 * (1 + Math.sin(theta / 180 * Math.PI))
+      data.push([r, theta])
+    }
+
+    myChart2.setOption(
+      {
+        title: {
+          text: '我知道你会来，所以我在等'
+        },
+        legend: {
+          data: ['line']
+        },
+        polar: {},
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        angleAxis: {
+          type: 'value',
+          startAngle: 0
+        },
+        radiusAxis: {
+        },
+        series: [{
+          coordinateSystem: 'polar',
+          name: 'line',
+          type: 'line',
+          data: data
+        }]
+      }
+    )
+  }
 
 }
 </script>
@@ -16,5 +63,9 @@ export default {
         // background-image: url('../../assets/img/home6.jpg');
         height: 100vh;
         background-size: cover;
+        .echarts{
+          width: 700px;
+          height: 700px;
+        }
     }
 </style>
